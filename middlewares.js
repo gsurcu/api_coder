4be9189fdb60553a828f04f0102420e20fd3185f
Gabriel Surculento <gespejo121@gmail.com>
2021-12-16 23:29:52 -0300

    Middlewares 2

--- middlewares.js
+++ middlewares.js
@@ -1,17 +1,26 @@
 const path = require('path')
 const express = require('express');
+const loggerMiddleware = require('./middlewares/logger');
+const authMiddleware =require('./middlewares/authorizer');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middlewares
+app.use([authMiddleware,loggerMiddleware]);
 
 // Rutas
 app.get('/', (req, res) => {
+  console.log(req.user)
   res.send('<h1>HOME</h1>')
 })
+
 app.get('/about', (req, res) => {
   res.send('<h1>ABOUT</h1>')
+})
+
+app.get('/api/products', (req, res) => {
+  res.send('<h1>PRODUCTS</h1>')
 })
 
 const connectedServer = app.listen(PORT, ()=> {
