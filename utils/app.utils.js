bdd08add3b861fe73dc8790344599b7c873c2b59
Gabriel Surculento <gespejo121@gmail.com>
2021-12-19 20:09:39 -0300

    Multer 3

--- utils/app.utils.js
+++ utils/app.utils.js
@@ -1,14 +1,13 @@
 const generarHtmlArchivos = (files) => {
   let html = '';
   files.map((file, index) => {
-    let filePath = file.path.split('public/')[1]
-    console.log(filePath);
+    let filePath = `${file.destination.split('public/')[1]}/${file.filename}`;
     const linkHtml = `<a href="http://localhost:8080/${filePath}">${file.fieldname}-${index+1}</a><br/>`;
     html += linkHtml
-  });
+  })
   return html;
 }
 
 module.exports = {
-  generarHtmlArchivos
+  generarHtmlArchivos,
 }
